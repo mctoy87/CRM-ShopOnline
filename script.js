2b73cc91d37c8f21b311365cb8d31a9a16412b65
mctoy87 <mctoy87@mail.ru>
2023-04-04 14:51:37 +0200

    fix the modal window whith grid and done first exercises whith js

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const nameProduct = 'Велосипед';
+const sumProduct = 10;
+const categoryProduct = 'Спорт';
+const priceProduct = 500;
+
+console.log(nameProduct);
+
+console.log(sumProduct*priceProduct);
