3735012f50d6b61cbef3684a052fdd4b2d93aea6
mctoy87 <mctoy87@mail.ru>
2023-05-26 16:04:27 +0200

    take elements from modal form

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const nameProduct = 'Велосипед';
-const sumProduct = 10;
-const categoryProduct = 'Спорт';
-const priceProduct = 500;
+const titlePage = document.querySelector('title');
+console.log(titlePage);
 
-console.log(nameProduct);
+const modalId = document.querySelector('.modal__id');
+console.log(modalId);
 
-console.log(sumProduct*priceProduct);
+const modalClose = document.querySelector('.modal__close');
+console.log(modalClose);
+
+const modalForm = document.querySelector('.modal__form');
+console.log(modalForm);
+
+const modalCheckbox = document.querySelector('.form__checkbox-label');
+console.log(modalCheckbox);
+
+const modalDiscountInput = document.querySelector('.form__input_disabled');
+console.log(modalDiscountInput);
+
+const modalTotalCost = document.querySelector('.form__total-cost');
+console.log(modalTotalCost);
